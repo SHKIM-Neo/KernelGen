f0a28b1f565beeb98c703bc85e45331d86aff899
SHKIM-Neo <shkim@neowine.com>
2021-09-13 10:18:49 +0900

    Changed project configuration

---
+++ kernelgen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    printf("test");
+}
